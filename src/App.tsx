af0cf32694d1113532bfc1f485727690e48abefc
Coordinate-Cat <tetrapasta02@gmail.com>
2024-05-09 15:14:02 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="text-red-400">
+      <h1 className="text-4xl text-red-100">Hello World</h1>
+      <p className="text-lg">This is a React app with Tailwind CSS</p>
+    </div>
+  );
+}
+
+export default App;
