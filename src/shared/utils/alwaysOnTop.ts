3eece73a2ae6d699a528bc712a2ee4876cbfe920
Coordinate-Cat <tetrapasta02@gmail.com>
2024-05-23 16:42:10 +0900

    feat: Add alwaysOnTop utility function for toggling the always on top view

---
+++ src/shared/utils/alwaysOnTop.ts
@@ -0,0 +1,15 @@
+import { appWindow } from "@tauri-apps/api/window";
+
+// alwaysOnTop enable or disable
+export const alwaysOnTop = async (
+  alwaysOnTopView: boolean,
+  setAlwaysOnTopView: (value: boolean) => void,
+) => {
+  if (alwaysOnTopView) {
+    await appWindow.setAlwaysOnTop(false);
+    setAlwaysOnTopView(false);
+  } else {
+    await appWindow.setAlwaysOnTop(true);
+    setAlwaysOnTopView(true);
+  }
+};
