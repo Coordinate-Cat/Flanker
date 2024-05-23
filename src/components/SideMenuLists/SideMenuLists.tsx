ba12c32edbda9365d54df8264bc1faf93fa7364a
Coordinate-Cat <tetrapasta02@gmail.com>
2024-05-23 16:40:45 +0900

    chore: Add SideMenuLists component for creating side menu lists

---
+++ src/components/SideMenuLists/SideMenuLists.tsx
@@ -0,0 +1,51 @@
+// const/Category.tsからCategoryを取得し、サイドメニューのリストを作成する
+import { Category } from "../../shared/const/Category";
+import { Tooltip } from "react-tooltip";
+import { useStore } from "../../shared/store/store";
+
+type Place = "right";
+
+export const SideMenuLists = () => {
+  const PLACES = ["right"];
+  const { setCurrentPage, currentPage } = useStore();
+  return (
+    <ul>
+      {Category.map((category, index) => (
+        <li
+          key={index}
+          data-tooltip-html={category}
+          data-tooltip-id="my-tooltip-data-html tooltip-anchor-show"
+          data-tooltip-delay-show={1000}
+          className={`cursor-pointer rounded-sm px-0.5 text-[8px] hover:text-[#d95e00] ${
+            currentPage === category ? "bg-[#111] text-[#d95e00]" : ""
+          }`}
+        >
+          <button
+            onClick={() => setCurrentPage(category)}
+            className="w-full truncate text-left"
+          >
+            {category}
+          </button>
+          {PLACES.map((place) => (
+            <Tooltip
+              key={place}
+              place={place as Place}
+              id="my-tooltip-data-html tooltip-anchor-show"
+              style={{
+                width: "fit-content",
+                maxWidth: "180px",
+                fontSize: "10px",
+                padding: "4px",
+                marginLeft: "0",
+                borderRadius: "4px",
+                backgroundColor: "#404040",
+                color: "#d95e00",
+                fontWeight: "bold",
+              }}
+            />
+          ))}
+        </li>
+      ))}
+    </ul>
+  );
+};
