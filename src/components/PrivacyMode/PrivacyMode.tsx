eea4aaf3595915ca8d2e661c8bf58b1e168292d4
Coordinate-Cat <tetrapasta02@gmail.com>
2024-05-23 16:40:16 +0900

    feat: Add PrivacyMode component

---
+++ src/components/PrivacyMode/PrivacyMode.tsx
@@ -0,0 +1,7 @@
+export const PrivacyMode = () => {
+  return (
+    <div className="flex w-full cursor-default flex-col items-center justify-center truncate text-[10px] italic mix-blend-difference">
+      <p className="w-fit select-none mix-blend-difference">Privacy Mode...</p>
+    </div>
+  );
+};
