13a930dd760e964b9678f4ea556c5ceb16658ad7
Coordinate-Cat <tetrapasta02@gmail.com>
2024-05-23 16:41:55 +0900

    feat: Add useResizeObserver hook for tracking SideMenu width

---
+++ src/shared/hooks/useResizeObserver.ts
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+
+// SideMenuの幅の数値を取得
+const useResizeObserver = (setSideMenuWidth: (width: number) => void) => {
+  useEffect(() => {
+    const sidemenu = document.getElementById("SideMenu");
+    if (sidemenu) {
+      const observer = new ResizeObserver((entries) => {
+        for (let entry of entries) {
+          setSideMenuWidth(entry.contentRect.width);
+        }
+      });
+      observer.observe(sidemenu);
+      return () => observer.disconnect();
+    }
+  }, [setSideMenuWidth]);
+};
+
+export default useResizeObserver;
