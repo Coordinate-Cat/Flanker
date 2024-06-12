5f22e8a07035706ebea0e8a4d0d39689aaa3968a
Coordinate-Cat <tetrapasta02@gmail.com>
2024-06-12 16:23:54 +0900

    feat: Add system information retrieval functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-#[tauri::command]
+
+mod system_info;
+
+use system_info::get_system_info;
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![get_system_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
